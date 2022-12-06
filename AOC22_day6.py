0ac3c26b1056ce0fafd3b7efa753efd885962acf
Aymane <64670363+polimeme@users.noreply.github.com>
2022-12-06 08:15:55 +0100

    Update AOC22_day6.py

--- AOC22_day6.py
+++ AOC22_day6.py
@@ -3,7 +3,7 @@ r=f.readline()
 print(r)
 #per parte 2 sostituire i 4 con 14
 def ez(r):
-    for n in range(0,len(r)-14):
-        if len(list(set(list(r[n:n+14]))))==14:
-            return n+14
+    for n in range(0,len(r)-4):
+        if len(list(set(list(r[n:n+4]))))==4:
+            return n+4
 print(ez(r))
